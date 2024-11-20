d1049e3c9d27217df5571baf06b85560cc3301cf
fardinariful <fardinewucse2020@gmail.com>
2024-11-20 11:41:56 +0600

    basic setup done

---
+++ src/Components/Registar/Registar.jsx
@@ -0,0 +1,9 @@
+const Registar = () => {
+    return (
+        <div>
+            <h1>This is Registar</h1>
+        </div>
+    );
+};
+
+export default Registar;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,29 @@ import {
 } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import Root from './Components/Root/Root.jsx';
+import Home from './Components/Home/Home.jsx';
+import Login from './Components/Login/Login.jsx';
+import Registar from './Components/Registar/Registar.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+      {
+        path:'/registar',
+        element:<Registar></Registar>
+      }
+    ]
   },
 ]);
 createRoot(document.getElementById('root')).render(
