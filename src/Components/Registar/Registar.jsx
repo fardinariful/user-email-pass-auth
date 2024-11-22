3168179f6e2cea4b06d36fa0e1999e88e48fe5ea
fardinariful <fardinewucse2020@gmail.com>
2024-11-22 21:44:59 +0600

    terms and conditon added

--- src/Components/Registar/Registar.jsx
+++ src/Components/Registar/Registar.jsx
@@ -1,18 +1,29 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
-
+import { FaEye,FaEyeSlash } from "react-icons/fa";
 const Registar = () => {
     const [Errormsg,setErrormsg]=useState('');
     const [success,setsuccess]=useState('');
+    const [showpass,setshowpass]=useState(false);
     const handleclickreg=e=>{
         e.preventDefault();
         const email=e.target.email.value;
         console.log(email);
         const password=e.target.password.value;
         console.log(password);
+        const accepted=e.target.terms.checked;
         if(password.length<6){
             setErrormsg('Password should be 6 cha');
+            return;
+        }
+        else if(!/[A-Z]/.test(password)){
+            setErrormsg('Password should be atleast one uppercase character');
+            return;
+        }
+        else if(!accepted){
+            setErrormsg("Please accept our terms and condition");
+            return;
         }
         setErrormsg('');
         setsuccess('');
@@ -36,7 +47,17 @@ const Registar = () => {
          <form className="w-1/2 space-y-3" onSubmit={handleclickreg}>
             <input className="text-black border border-red-600 w-full p-4" type="email" name="email" id="" placeholder="Email Address" />
             <br />
-            <input className="text-black border border-red-600 w-full p-4" type="password" name="password" id="" placeholder="password"/>
+            <div className="relative">
+            <input className="text-black border border-red-600 w-full p-4" 
+            type={showpass?"text":"password"} 
+            name="password" id="" placeholder="password"/>
+            <span className="absolute top-5 right-3" onClick={()=>setshowpass(!showpass)}>
+                {showpass ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+                </span>
+            </div>
+            <br />
+            <input type="checkbox" name="terms" id="terms" />
+            <label className="ml-2" htmlFor="">Accept Terms and Condition</label>
             <br />
             <input className="btn btn-primary  w-full p-4" type="submit" value="Register" />
          </form>
